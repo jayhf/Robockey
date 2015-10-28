c3dd595a06240520dc0e799c9744fdd00624c205
Jay Fleischer <jayhf614@gmail.com>
2015-10-27 21:42:31 -0400

    Updated .gitignore and created a main file

---
+++ Acrobot/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Acrobot.cpp
+ *
+ * Created: 10/27/2015 9:33:04 PM
+ * Author : Jay
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
