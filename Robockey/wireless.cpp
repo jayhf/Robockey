e1017926df38536e06b6beb04977c8c643e069d1
Jay Fleischer <jayhf614@gmail.com>
2015-12-05 03:01:12 -0500

    Fixed indentation

--- Robockey/wireless.cpp
+++ Robockey/wireless.cpp
@@ -103,44 +103,36 @@ void updateWireless(){
 		uint8_t buffer[10];
 		m_rf_read((char*)buffer,10);
 		switch(buffer[0]){
-			case 0x20:
-			//Set variable
-			break;
-			case 0x21:
-			//Send back requested variable
-			break;
 			case static_cast<uint8_t>(Robot::ROBOT1):
 			case static_cast<uint8_t>(Robot::ROBOT2):
 			case static_cast<uint8_t>(Robot::ROBOT3):
-			{
 				//Received a message from another team robot
 				processTeamMessage(getAlly(static_cast<Robot>(buffer[0])),buffer);
 				break;
-			}
 			case 0xA0:
-			updateGameState(GameState::COMM_TEST);
-			break;
+				updateGameState(GameState::COMM_TEST);
+				break;
 			case 0xA1:
-			updateGameState(GameState::PLAY);
-			break;
+				updateGameState(GameState::PLAY);
+				break;
 			case 0xA2:
-			goalScored(Team::RED);
-			updateScores(buffer[0],buffer[1]);
-			break;
+				goalScored(Team::RED);
+				updateScores(buffer[0],buffer[1]);
+				break;
 			case 0xA3:
-			goalScored(Team::BLUE);
-			updateScores(buffer[0],buffer[1]);
-			break;
+				goalScored(Team::BLUE);
+				updateScores(buffer[0],buffer[1]);
+				break;
 			case 0xA4:
-			updateGameState(GameState::PAUSE);
-			break;
+				updateGameState(GameState::PAUSE);
+				break;
 			case 0xA6:
-			updateGameState(GameState::HALFTIME);
-			break;
+				updateGameState(GameState::HALFTIME);
+				break;
 			case 0xA7:
-			updateGameState(GameState::GAME_OVER);
-			break;
-			case 0xA8:
+				updateGameState(GameState::GAME_OVER);
+				break;
+			//case 0xA8:
 			//receivedEnemyLocations((int8_t*)(buffer + 1));
 			break;
 		}
