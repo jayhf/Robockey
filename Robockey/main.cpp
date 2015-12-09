332e5b1c782c4d788a8bc2a20b8a06f036293366
Jay Fleischer <jayhf614@gmail.com>
2015-12-09 17:12:55 -0500

    Kick test code

--- Robockey/main.cpp
+++ Robockey/main.cpp
@@ -207,7 +207,7 @@ void pool(){
 				break;
 		}
 		//setMotors(0,0);
-		if (allowedToMove()){
+		if (1|allowedToMove()){
 			if(getRobotPose()==UNKNOWN_POSE){
 				setMotors(0,0);
 			}
@@ -217,7 +217,8 @@ void pool(){
 				}
 				else{*/
 					if (first == 0) first++;
-					updateStrategies();
+					tryKick();
+					//updateStrategies();
 					//defenseLogic3();
 				}
 			//}
