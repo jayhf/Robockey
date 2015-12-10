c051d3c67a0f184c661c22f076e9188a8745db3f
danielorol <danielhorol@gmail.com>
2015-12-10 06:25:32 -0500

    the final countdown

--- Robockey/main.cpp
+++ Robockey/main.cpp
@@ -207,7 +207,7 @@ void pool(){
 				break;
 		}
 		//setMotors(0,0);
-		if (1){
+		if (allowedToMove()){
 			if(getRobotPose()==UNKNOWN_POSE){
 				setMotors(0,0);
 			}
@@ -217,7 +217,7 @@ void pool(){
 				}
 				else{*/
 					if (first == 0) first++;
-					defenseLogic3();
+					playerLogic(getPlayer());
 				}
 			//}
 		}
